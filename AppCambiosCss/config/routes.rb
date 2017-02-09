851a0c64a9a91ab0aa8cc93667f06a52fcfc0146
sebastian quevedo <sebaqd91@gmail.com>
2017-02-09 16:37:52 -0300

    prueba nueva consola

---
+++ AppCambiosCss/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  get 'home/page1'
+
+  get 'home/page2'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
